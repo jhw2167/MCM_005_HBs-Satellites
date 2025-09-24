f82ad2944109bf3ecc8e87860221945a6fc33d4c
jhw2167 <jhw2167@columbia.edu>
2025-09-24 18:50:12 -0500

    refactor: Increase satellite map capacity and add SatelliteDisplay initialization

--- common/src/main/java/com/holybuckets/satellite/core/SatelliteManager.java
+++ common/src/main/java/com/holybuckets/satellite/core/SatelliteManager.java
@@ -4,17 +4,26 @@ import com.holybuckets.foundation.event.EventRegistrar;
 import com.holybuckets.satellite.block.be.SatelliteBlockEntity;
 import io.netty.util.collection.IntObjectHashMap;
 import net.blay09.mods.balm.api.event.server.ServerStoppedEvent;
+import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.chunk.LevelChunk;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SatelliteManager {
 
     /** Maps colorId to satellite block entity */
-    public static final IntObjectHashMap<SatelliteBlockEntity> SATELLITES = new IntObjectHashMap<>(16);
+    public static final IntObjectHashMap<SatelliteBlockEntity> SATELLITES = new IntObjectHashMap<>(24);
+
+    private static final Map<ChunkPos, LevelChunk> CHUNK_CACHE = new HashMap<>();
 
     public static void init(EventRegistrar reg) {
         reg.registerOnServerStopped(SatelliteManager::onServerStopped);
-    }
 
+        //SatelliteDisplay
+        SatelliteDisplay.init(reg);
+    }
 
     public static SatelliteBlockEntity get(int colorId) {
         return SATELLITES.get(colorId);
